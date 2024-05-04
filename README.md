6d13100bd2e322f43d9dbcee913d4ef72925d854
spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-05-04 14:33:00 +0300

    add scripts, readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Install tarball version of Visual Studio Code with just a basic script!
+
+A script to easily install Visual Studio Code the tarball way on your Linux machine!
+
+Note: This installation script is by no means affiliated with Visual Studio Code project, or its maintainers.
+
+## Usage
+
+Clone the repo and run the script
+```bash
+
+git clone https://github.com/spookyorange/vscode-linux-install.git
+cd vscode-linux-install
+sh ./install.sh
+
+```
+
+To remove the application(if it has been installed with this method)
+```bash
+
+sh ./uninstall.sh
+
+```
+
+Updates are handled by running the install script again, you can just run the install script and all good!
+```bash
+
+sh ./install.sh
+
+```
+
+## Details
+
+The script will install the application in the following destinations if you have installed locally:
+
+- ~/.tarball-installations/code
+- ~/.local/bin/code
+- ~/.local/share/applications/code.desktop
+
+## Tested Distros
+
+- Fedora by Spookyorange
+- Fedora Atomic(Silverblue) by Spookyorange
+- SteamOS(Steam Deck) by Spookyorange
+
+## Contributing
+
+If you have a distro that you would like to add to the list of tested distros, please submit a pull request with the changes you made to the script and the distro you tested it on.
+
+
---
+++ install.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+app_name="code"
+display_name="Visual Studio Code"
+literal_name_of_installation_directory=".tarball-installations"
+universal_path_for_installation_directory="$HOME/$literal_name_of_installation_directory"
+app_installation_directory="$universal_path_for_installation_directory/$app_name"
+official_package_location="https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
+tar_location="./hello.tar.gz"
+local_bin_path="$HOME/.local/bin"
+local_application_path="$HOME/.local/share/applications"
+app_bin_in_local_bin="$local_bin_path/$app_name"
+desktop_in_local_applications="$local_application_path/$app_name.desktop"
+icon_path=$app_installation_directory/resources/app/resources/linux/code.png
+executable_path=$app_installation_directory/code
+
+desktop_keywords=web,development,code,api,text,editor
+desktop_terminal=false
+desktop_type=Application
+desktop_categories="Api;Code;Development;Text;Edit;Editor;"
+desktop_startup_wm_class="Visual Studio Code"
+desktop_exec="$executable_path %u"
+desktop_actions="new-empty-window;"
+desktop_other_actions="
+[Desktop Action new-empty-window]
+Name=New Empty Window
+Exec=code --new-window %F
+Icon=com.visualstudio.code
+"
+
+echo "Welcome to $display_name tarball installer, just chill and wait for the installation to complete!"
+
+sleep 1
+
+echo "Checking if old files exist"
+if [ -f $app_bin_in_local_bin ]; then
+  echo "Old bin file detected, removing..."
+  rm $app_bin_in_local_bin
+fi
+
+if [ -d $app_installation_directory ]; then
+  echo "Old app files are found, removing..."
+  rm -rf $app_installation_directory
+fi
+
+if [ -f $desktop_in_local_applications ]; then
+  echo "Old bin file detected, removing..."
+  rm $desktop_in_local_applications
+fi
+
+sleep 1
+
+echo "Installing the latest package"
+curl -L -o $tar_location $official_package_location
+
+mkdir $app_name
+
+tar -xvf $tar_location -C $app_name --strip-components=1
+
+echo "Installed and untarred successfully"
+
+if [ ! -d $universal_path_for_installation_directory ]; then
+  echo "Creating the $universal_path_for_installation_directory directory for installation"
+  mkdir $universal_path_for_installation_directory
+fi
+
+mv $app_name $app_installation_directory
+
+echo "$app_name successfully moved to your safe place!"
+
+rm $tar_location
+
+if [ ! -d $local_bin_path ]; then
+  echo "$local_bin_path not found, creating it for you"
+  mkdir $local_bin_path
+fi
+
+touch $app_bin_in_local_bin
+chmod u+x $app_bin_in_local_bin
+echo "#!/bin/bash
+$executable_path" >> $app_bin_in_local_bin
+
+echo "Created executable for your \$PATH if you ever need"
+
+if [ ! -d $local_application_path ]; then
+  echo "Creating the $local_application_path directory for desktop file"
+  mkdir $local_application_path
+fi
+
+touch $desktop_in_local_applications
+echo "
+[Desktop Entry]
+Name=$display_name
+Keywords=$desktop_keywords
+Exec=$desktop_exec
+Icon=$icon_path
+Terminal=$desktop_terminal
+Type=$desktop_type
+StartupWMClass=$desktop_startup_wm_class
+Categories=$desktop_categories
+Actions=$desktop_actions
+
+$desktop_other_actions
+" >> $desktop_in_local_applications
+
+echo "Created desktop entry successfully"
+
+sleep 1
+
+echo "Installation is successful"
+
+sleep 1
+
+echo "Done, and done, have fun!"
+
+sleep 1
+
+exit 0
+
+
